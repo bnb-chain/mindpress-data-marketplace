2f0082423c4d3a49fc3e0b92ef5acd7e7e606cd0
ybgbob <bob.g@nodereal.io>
2024-01-17 15:12:59 +0800

    feat: Link Style

--- src/components/home/Links.tsx
+++ src/components/home/Links.tsx
@@ -20,87 +20,78 @@ export const Links = () => {
         <CardTitle>What Makes Us Different?</CardTitle>
       </TitleCon>
       <CardCon gap={24}>
-        <Box
-          onClick={() => {
-            window.open(
-              'https://docs.bnbchain.org/greenfield-docs/docs/guide/home',
-              '_blank',
-            );
-          }}
+        <CardItem
+          gap={16}
+          flexDirection={'column'}
+          // onClick={() => {
+          //   window.open(
+          //     'https://docs.bnbchain.org/greenfield-docs/docs/guide/home',
+          //     '_blank',
+          //   );
+          // }}
         >
-          <CardItem gap={16} flexDirection={'column'}>
-            <Box className="icon">
-              <StorageIcon w={32} h={32} />
-            </Box>
-            <CardItemTitle>Decentralized Storage</CardItemTitle>
-            <Box className="desc">
-              All your files are storage on BNB Greenfield decentralized storage
-              network Ownership on Blockchain
-            </Box>
-          </CardItem>
-        </Box>
-        <Box
-          onClick={() => {
-            window.open('https://github.com/bnb-chain/greenfield', '_blank');
-          }}
+          <Box className="icon">
+            <StorageIcon w={32} h={32} />
+          </Box>
+          <CardItemTitle>Decentralized Storage</CardItemTitle>
+          <Box className="desc">
+            All your files are storage on BNB Greenfield decentralized storage
+            network Ownership on Blockchain
+          </Box>
+        </CardItem>
+        <CardItem
+          gap={16}
+          flexDirection={'column'}
+          // onClick={() => {
+          //   window.open('https://github.com/bnb-chain/greenfield', '_blank');
+          // }}
         >
-          <CardItem gap={16} flexDirection={'column'}>
-            <Box className="icon">
-              <CopyrightIcon w={32} h={32} />
-            </Box>
-            <CardItemTitle>Copyright on Blockchain</CardItemTitle>
-            <Box className="desc">
-              Fully ownership and permission control managed by decentralized
-              blockchain Data Security
-            </Box>
-          </CardItem>
-        </Box>
-        <Box
-          onClick={() => {
-            window.open(
-              'https://greenfield.bnbchain.org/en/bridge?type=transfer-in',
-              '_blank',
-            );
-          }}
+          <Box className="icon">
+            <CopyrightIcon w={32} h={32} />
+          </Box>
+          <CardItemTitle>Copyright on Blockchain</CardItemTitle>
+          <Box className="desc">
+            Fully ownership and permission control managed by decentralized
+            blockchain Data Security
+          </Box>
+        </CardItem>
+        <CardItem
+          gap={16}
+          flexDirection={'column'}
+          // onClick={() => {
+          //   window.open(
+          //     'https://greenfield.bnbchain.org/en/bridge?type=transfer-in',
+          //     '_blank',
+          //   );
+          // }}
         >
-          <CardItem gap={16} flexDirection={'column'}>
-            <Box className="icon">
-              <LockIcon w={32} h={32} color="#FFE900" />
-            </Box>
-            <CardItemTitle>Data Security</CardItemTitle>
-            <Box className="desc">
-              Data is stored in a decentralized way with backups User Experience
-            </Box>
-          </CardItem>
-        </Box>
-        <Box
-          onClick={() => {
-            window.open('https://discord.com/invite/QRTQvfhADQ', '_blank');
-          }}
+          <Box className="icon">
+            <LockIcon w={32} h={32} color="#FFE900" />
+          </Box>
+          <CardItemTitle>Data Security</CardItemTitle>
+          <Box className="desc">
+            Data is stored in a decentralized way with backups User Experience
+          </Box>
+        </CardItem>
+        <CardItem
+          gap={16}
+          flexDirection={'column'}
+          // onClick={() => {
+          //   window.open('https://discord.com/invite/QRTQvfhADQ', '_blank');
+          // }}
         >
-          <CardItem gap={16} flexDirection={'column'}>
-            <Box className="icon">
-              <UserIcon w={32} h={32} />
-            </Box>
-            <CardItemTitle>Seamless User Experience</CardItemTitle>
-            <Box className="desc">
-              Seamless user experience to boost photo trading
-            </Box>
-          </CardItem>
-        </Box>
+          <Box className="icon">
+            <UserIcon w={32} h={32} />
+          </Box>
+          <CardItemTitle>Seamless User Experience</CardItemTitle>
+          <Box className="desc">
+            Seamless user experience to boost photo trading
+          </Box>
+        </CardItem>
       </CardCon>
     </Flex>
   );
 };
-
-const Cards = styled(Flex)`
-  /* margin: 80px 0 114px; */
-  background-color: #181a1e;
-  width: 1400px;
-  /* height: 426px; */
-  border-radius: 32px;
-  border: 2px solid #1e2026;
-`;
 
 const TitleCon = styled(Flex)``;
 
@@ -110,7 +101,9 @@ const CardTitle = styled.div`
   color: #ffffff;
 `;
 
-const CardCon = styled(Flex)``;
+const CardCon = styled(Flex)`
+  /* align-items: stretch; */
+`;
 
 const CardItem = styled(Flex)`
   width: 282px;
@@ -132,10 +125,10 @@ const CardItem = styled(Flex)`
   }
   .desc {
   }
-  &:hover {
+  /* &:hover {
     background-color: #373943;
     cursor: pointer;
-  }
+  } */
 `;
 
 const CardItemTitle = styled.div`
